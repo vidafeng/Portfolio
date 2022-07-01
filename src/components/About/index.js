b83ee66b4be1f2eafdb29fe0dfe446d6cf392373
Vida Feng <vidaffeng@gmail.com>
2022-07-01 17:00:29 -0400

    removed gsap

---
+++ src/components/About/index.js
@@ -0,0 +1,28 @@
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const About = () => {
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+            idx={15}
+          />
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,36 +1,10 @@
 import './index.scss'
 import LogoV from '../../../assets/images/logo-v.png'
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 
 const Logo = () => {
-  const bgRef = useRef()
-  const solidLogoRef = useRef()
-
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-    gsap.timeline().to(bgRef.current, {
-      duration: 1,
-      opacity: 1,
-    })
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 2,
-      }
-    )
-  }, [])
-
   return (
-    <div className="logo-container" ref={bgRef}>
-      <img ref={solidLogoRef} className="solid-logo" src={LogoV} alt="V" />
+    <div className="logo-container">
+      <img className="solid-logo" src={LogoV} alt="V" />
     </div>
   )
 }
