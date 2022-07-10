c959db92c0515579c8b75bce295c218d04198517
Vida Feng <vidaffeng@gmail.com>
2022-07-10 19:25:28 -0400

    added loaders and hover animations

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,35 +2,71 @@ import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import './index.scss'
 import Headshot from '../../assets/images/headshot.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faReact,
+  faCss3,
+  faHtml5,
+  faJsSquare,
+  faGitAlt,
+  faNode,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
   useEffect(() => {
     setTimeout(() => {
+      // update class after 3 seconds
       setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
-  return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          Hello, world! I'm Vida, and I'm a Software Engineer passionate about
-          developing scalable, impactful solutions to real-world problems.
-        </p>
-      </div>
 
-      <div className="headshot-container">
-        <img alt="vida headshot" className="headshot" src={Headshot} />
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Hello, world! I'm Vida, and I'm a Software Engineer passionate about
+            developing scalable, impactful solutions to real-world problems.
+          </p>
+        </div>
+
+        <div className="headshot-container">
+          <img alt="vida headshot" className="headshot" src={Headshot} />
+
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faNode} color="#68A063" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import LogoTitle from '../../assets/images/logo-v.png'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
   // state hook
@@ -34,38 +35,41 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          {/* delay of 12 or 1.2s delay */}
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            {/* delay of 12 or 1.2s delay */}
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
 
-          <img src={LogoTitle} alt="developer"></img>
+            <img src={LogoTitle} alt="developer"></img>
 
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          {/* web developer */}
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>Software Engineer</h2>
-        <Link to="/contact" className="flat-button">
-          LET'S CONNECT
-        </Link>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            {/* web developer */}
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Software Engineer</h2>
+          <Link to="/contact" className="flat-button">
+            LET'S CONNECT
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
